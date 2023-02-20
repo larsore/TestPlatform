b4036071105168854b62c2cef932807038f3c7ae
Vegard Nyeng <vegardnyeng@dhcp-10-22-68-19.wlan.ntnu.no>
2023-02-20 11:06:04 +0100

    Prototype med websockets. Mangler litt formatering og verifikasjon

---
+++ testbed_client.py
@@ -0,0 +1,69 @@
+import asyncio
+import websockets
+import numpy as np
+import sympy as sy
+import struct
+
+server = "ws://localhost:8765"
+
+q = sy.randprime(2**31, 2**(32)-1)
+m = 4 #rader
+n = 12 #kolonner
+beta = 30
+
+
+#Convert vectors and matrices to binary 
+
+def toBinary(data):
+    if type(data) is np.ndarray: #if vector or matrix
+        binaryData = data.tobytes()
+    else:
+        binaryData = struct.pack('%sl' % len(data), *data)
+    return binaryData
+
+
+#Prover generates A and secret s
+A = np.random.randint(low=0, high=q, size=(n, m))
+s = np.random.randint(low=-1, high=2, size = m)
+
+#Convert matrix A to binary for sending to server
+binaryA = toBinary(A)
+
+
+#Prover calculates public key t
+t = (A.dot(s))%q
+binaryT = toBinary(t)
+
+
+y = np.random.randint(low=-1, high=2, size=m)
+
+#Committment w
+w = (A.dot(y))%q
+binaryW = toBinary(w)
+
+
+
+
+async def onMessage():
+    async with websockets.connect(server) as websocket:
+        await websocket.send(binaryA)
+        print("Sent A = \n", str(A),"\n")
+
+        await websocket.send(binaryT)
+        print("Sent t = ", t,"\n")
+
+        await websocket.send(binaryW)
+        print("Sent w = ", w,"\n")
+
+        challenge = await websocket.recv()
+        print("Challenge from server: ", challenge,"\n")
+
+        z = (s*int(challenge) + y)
+        binaryZ = toBinary(z)
+        await websocket.send(binaryZ)
+        print("sent z: ", z,"\n")
+        
+       
+    
+
+asyncio.run(onMessage())
---
+++ testbed_server.py
@@ -0,0 +1,94 @@
+import asyncio
+import websockets
+import numpy as np
+import sympy as sy
+import struct
+
+client = "ws://localhost:8765"
+
+q = sy.randprime(2**31, 2**(32)-1)
+
+"""
+def unpack(binary_data):
+    # convert binary data to float values
+    integer_values = struct.unpack('%sl' % len(binary_data)//4, binary_data)
+
+    # convert float values to a list
+    data = list(integer_values)
+
+    return data
+
+
+def unpack_vector(binary_vector, dtype=np.int32):
+    '''unpack binary representation of a vector into a numpy array'''
+    size = len(binary_vector) // struct.calcsize(dtype)
+    return np.array(struct.unpack(f'{size}{dtype.char}', binary_vector), dtype=dtype)
+
+def unpack_matrix(binary_matrix, dtype=np.int32, shape=None):
+    '''unpack binary representation of a matrix into a numpy array'''
+    if shape is None:
+        raise ValueError('shape must be specified')
+    rows, cols = shape
+    size = rows * cols
+    binary_vector = struct.unpack(f'{size}{dtype.any}', binary_matrix)
+    return np.array(binary_vector, dtype=dtype).reshape(shape)
+"""
+
+
+def verification(A,z,t,c,w,q):
+    leftSide = (A.dot(z))%q
+    rightSide = (t*c+w)%q
+
+    if leftSide.np.array_equal(rightSide):
+        print("JA!")
+
+    if (rightSide.any() != leftSide.any()):
+        raise ValueError('Az != t*c + w')
+    else: 
+        print("SUCCESS")
+
+#Trengs én handler per connection
+async def handler(websocket):
+    while True:
+        try:    
+            binaryA = await websocket.recv()
+            A = np.frombuffer(binaryA, dtype = int)
+
+            binaryT = await websocket.recv()
+            t = np.frombuffer(binaryT, dtype = int)
+
+            binaryW = await websocket.recv()
+            w = np.frombuffer(binaryW, dtype = int)
+
+            c = np.random.randint(0,q-1)
+            await websocket.send(str(c))
+
+            binaryZ = await websocket.recv()
+            z = np.frombuffer(binaryZ, dtype = int)
+
+        except websockets.ConnectionClosedOK:
+            break
+
+        print("Received Matrix A from client : \n," , A, "\n")
+        print("t from client: ", t, "\n")
+        print("w from clent: ", w, "\n")
+        print("z from client: ", z)
+
+        verification(A,z,t,c,w,q)
+        
+
+
+
+
+async def main():
+    async with websockets.serve(handler, "localhost", 8765):
+        await asyncio.Future() #server runs until manually stopped
+
+
+
+
+if __name__ == "__main__":
+    asyncio.run(main())
+
+
+
