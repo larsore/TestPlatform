11ab346f43f82df212e574848197995cafb5e68f
Vegard Nyeng <vegardnyeng@Vegards-MacBook-Pro.local>
2023-03-09 17:15:53 +0100

    Lagt til routing, startet på registrering av bbruker

--- api.py
+++ api.py
@@ -4,7 +4,9 @@ import socketserver
 from typing import Tuple
 from http import HTTPStatus
 import socket
-
+import numpy as np
+import secrets
+from routes.routes import routes
 
 class Handler(http.server.SimpleHTTPRequestHandler):
 
@@ -12,15 +14,46 @@ class Handler(http.server.SimpleHTTPRequestHandler):
         super().__init__(request, client_address, server)
 
     @property
-    def api_response(self):
-        return json.dumps({"message": "Hello world"}).encode()
-
+    def challenge_response(self):
+        ch = str(secrets.token_bytes(10))
+        challenge = np.random.randint(0,1000)
+        return json.dumps({"challenge": challenge}).encode()
+    
     def do_GET(self):
-        if self.path == 'api/':
+        self.send_response(HTTPStatus.OK)
+        self.send_header("Content-type", "application/json")
+        self.end_headers()
+        if self.path in routes:
+            route_content = routes[self.path]
+            return self.wfile.write(b'%s' % route_content.encode())
+        else:
+            return self.wfile.write(b'%s is not a valid path' % self.path.encode())
+
+        
+    def do_POST(self):
+        if self.path == '/register':
             self.send_response(HTTPStatus.OK)
-            self.send_header("Content-Type", "application/json")
+            self.data_string = self.rfile.read(int(self.headers['Content-Length']))
+            registerRequest = json.loads(self.data_string.decode('utf8').replace("'", '"'))
+            print(registerRequest)
+            self.send_header("Content-type", "application/json")
             self.end_headers()
-            self.wfile.write(bytes(self.api_response))
+
+            #TODO check if username already exists. If it exists: return message telling user that it is taken, else: add username to database and return message to user telling that user with username x is successully registered.
+            if registerRequest.get("username") not in database:
+                return
+            return self.wfile.write(b'Du er naa registrert med brukernavn og authenticator %s' % self.data_string)
+        else:
+            return self.wfile.write(b'%s is not a valid path' % self.path.encode())
+        
+        
+
+        
+        
+
+        
+    
+
 
 
 if __name__ == "__main__":
---
+++ routes/routes.py
@@ -0,0 +1,5 @@
+routes = {
+    "/" : "Hello World! \n " + "Prøv /register for registrering og /auth for autentisering",
+    "/register" : "Her kommer logikk for registrering",
+    "/auth": "Her kommer logikk for autentisering"
+}
