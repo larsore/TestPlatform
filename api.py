c82e8542576c845dbe33fda3592cd73d60e55d6d
Vegard Nyeng <vegardnyeng@Vegards-MacBook-Pro.local>
2023-03-13 13:50:12 +0100

    updated register and auth

--- api.py
+++ api.py
@@ -14,12 +14,9 @@ class Handler(http.server.SimpleHTTPRequestHandler):
     def __init__(self, request: bytes, client_address: Tuple[str, int], server: socketserver.BaseServer):
         super().__init__(request, client_address, server)
 
-
-    @property
-    def challenge_response(self):
-        ch = str(secrets.token_bytes(10))
+    def challenge(self):
         challenge = np.random.randint(0,1000)
-        return json.dumps({"challenge": challenge}).encode()
+        return challenge
     
     def do_GET(self):
         self.send_response(HTTPStatus.OK)
@@ -33,6 +30,7 @@ class Handler(http.server.SimpleHTTPRequestHandler):
 
         
     def do_POST(self):
+        #Registrering
         if self.path == '/register':
             self.send_response(HTTPStatus.OK)
             self.data_string = self.rfile.read(int(self.headers['Content-Length']))
@@ -41,25 +39,57 @@ class Handler(http.server.SimpleHTTPRequestHandler):
             self.send_header("Content-type", "application/json")
             self.end_headers()
 
-            username = registerRequest.get("username")
-            authenticatorNickname = registerRequest.get("authenticator_nickname")
             dbClient = pymongo.MongoClient(('mongodb://localhost:27017/'))
             db = dbClient['FIDOServer']
             userColumn = db['Users']
 
+            username = registerRequest.get("username")
+            authenticatorNickname = registerRequest.get("authenticator_nickname")
             checkUserExistence = userColumn.find_one({'_id': username})
+
             if checkUserExistence == None: #No instance of that username in database
                 doc = {
                     '_id': username,
                     'authenticator_nickname': authenticatorNickname
                 }
                 regUser = userColumn.insert_one(doc)
-                return self.wfile.write(b'Du er naa registrert med brukernavn %s' % regUser.inserted_id.encode())
+                challenge = challenge()
+                rpID = self.rpID
+                return self.wfile.write(b"Du er naa registrert med brukernavn '%s'" % regUser.inserted_id.encode()) #TODO fjern dette når credential er ferdig laget og returnert
+            
+                
+                #TODO lag challenge, credID
+                #TODO returner challenge, credID og rpID
             else:
-                return self.wfile.write(b'Brukernavn %s er allerede i bruk, prov med et nytt' % registerRequest.get("username").encode())
+                #TODO legg til sjekk på username + authenticator? Skal egentil være mulig å registrere seg flere ganger med samme brukernavn men med ny authenticator
+                return self.wfile.write(b"Brukernavn '%s' er allerede i bruk, prov med et nytt" % registerRequest.get("username").encode())
+        
+        
+        #Autentisering
+        elif self.path == '/auth':
+            self.send_response(HTTPStatus.OK)
+            self.send_header("Content-type", "application/json")
+            self.end_headers()
+            self.data_string = self.rfile.read(int(self.headers['Content-Length']))
+
+            if self.data_string == "": #TODO check if request is malformed
+                self.send_response(HTTPStatus.BAD_REQUEST)
+            else:
+                dbClient = pymongo.MongoClient(('mongodb://localhost:27017/'))
+                db = dbClient['FIDOServer']
+                userColumn = db['Users']
+
+                authRequest = json.loads(self.data_string.decode('utf8').replace("'", '"'))
+                username = authRequest.get("username")
+                checkUserExistence = userColumn.find_one({'_id': username})
+
+                if checkUserExistence == None: #No instance of that username in database
+                    return self.wfile.write(b"No user with the username '%s' exists" % username.encode())
+                else:
+                    return self.wfile.write(b'Her kommer rpID, credID og challenge')  #TODO return rdID, credID,challenge. CredID ligger i database sammen med brukernavn, ble laget under registrering. 
         else:
             return self.wfile.write(b'%s is not a valid path' % self.path.encode())
-
+       
 
 if __name__ == "__main__":
     PORT = 8000
