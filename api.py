e6dc283ce921fac300fb2c4bfac629ec9ce4f440
Vegard Nyeng <vegardnyeng@dhcp-10-22-68-52.wlan.ntnu.no>
2023-03-15 11:58:47 +0100

    Buxfixes + ferdig med register/verification

--- api.py
+++ api.py
@@ -13,15 +13,16 @@ from hashlib import sha256
 
 
 class Handler(http.server.SimpleHTTPRequestHandler):
+    challenge = None
+    rpID = 1 #TODO skal være en url?
+    clientAddress = None
 
-    username = None
     def __init__(self, request: bytes, client_address: Tuple[str, int], server: socketserver.BaseServer):
         super().__init__(request, client_address, server)
        
-
-
     #TODO lag truly random challenge
-    def challenge(self):
+
+    def createChallenge(self):
         challenge = np.random.randint(0,1000)
         return challenge
 
@@ -47,6 +48,14 @@ class Handler(http.server.SimpleHTTPRequestHandler):
     def getChallenge(cls):
         return cls.challenge
     
+    @classmethod
+    def setClientAddress(cls, clientAddress):
+        cls.clientAddress = clientAddress
+    
+    @classmethod
+    def getClientAddress(cls):
+        return cls.clientAddress
+    
    
     def do_GET(self):
         self.send_response(HTTPStatus.OK)
@@ -61,25 +70,27 @@ class Handler(http.server.SimpleHTTPRequestHandler):
         
     def do_POST(self):
        
-        rpID = 1 #TODO skal være en url?
-
         #Registrering
         if self.path == '/register':
             registerRequest = json.loads(self.rfile.read(int(self.headers['Content-Length'])))
-            #self.data_string = self.rfile.read(int(self.headers['Content-Length']))
+            requestReceivedFrom = self.client_address[0]
+
+            self.setClientAddress(requestReceivedFrom) #Henter ip-adressen requesten ble sendt fra, setter denne globalt
+            print("Set client address to:",requestReceivedFrom)
+
             if registerRequest == b'': #Hvis body er tom, return 400 bad request
                 return self.send_error(400, "Bad request")
             else:
                 self.send_response(HTTPStatus.OK)
                 #registerRequest = json.loads(self.data_string.decode('utf8').replace("'", '"'))
-                print(registerRequest)
+                print("Recieved request from client:",registerRequest)
                
                 self.send_header("Content-type", "application/json") #TODO sende host her? sjekk hvilke headers som er mulige å sende
                 self.end_headers()
 
                 dbClient, db, userCollection = self.startDatabase()
               
-                challenge = self.challenge() 
+                challenge = self.createChallenge() 
                 self.setChallenge(challenge=challenge)
                 self.expectedClientAddress = self.client_address[0] 
                 username = registerRequest.get("username") #avhengig av at kun én pers registrerer seg og verfifiserer seg om gangen. #TODO se på alternativ løsning
@@ -121,7 +132,7 @@ class Handler(http.server.SimpleHTTPRequestHandler):
                             }
                         }
                     }
-                    print("hash:", sha256(str(rpID+self.getChallenge()).encode()).hexdigest())
+                    print("hash:", sha256(str(self.rpID+self.getChallenge()).encode()).hexdigest())
                     return self.wfile.write(json.dumps(cred).encode()) #TODO sende host slik at client kan sjekke host==rpID
                 else:
                     self.send_error(409, "Username taken")
@@ -149,15 +160,17 @@ class Handler(http.server.SimpleHTTPRequestHandler):
 
                 challenge = self.getChallenge()
 
-                verifyClientAddress = self.client_address[0] == "192.168.0.44" or self.client_address[0] == "127.0.0.1"
-                verifyClientData = request.get("client_data") == sha256(str(rpID+challenge).encode()).hexdigest()
+                expectedClientAddress = self.getClientAddress()
+
+                verifyClientAddress = expectedClientAddress == self.client_address[0]
+                #verifyClientAddress = self.client_address[0] == "192.168.0.44" or self.client_address[0] == "127.0.0.1"
+                verifyClientData = request.get("client_data") == sha256(str(self.rpID+challenge).encode()).hexdigest()
                 verifyPublicKey = "public_key" in request
 
-                
-
+                print("expected client address:",expectedClientAddress)
                 print("actual client address:",self.client_address[0])
 
-                if verifyClientAddress and verifyClientData and verifyPublicKey: #self.expectedClientAddress: #TODO finn en måte å hente ut origin fra http request på
+                if verifyClientAddress and verifyClientData and verifyPublicKey:
                     username = self.getUsername()
                     """
                     doc = {
@@ -166,7 +179,6 @@ class Handler(http.server.SimpleHTTPRequestHandler):
                     'publicKey': request.get("public_key")
                     } 
                     """#TODO finn ut om det er mulig å legge inn flere fields samtidig i collection
-                    print("self.username:",username)
 
                     userCollection.find_one_and_update({"_id": username}, {"$set": {"credential_id": request.get("credential_id"), "public_key": request.get("public_key")}})
 
@@ -175,10 +187,14 @@ class Handler(http.server.SimpleHTTPRequestHandler):
                         print(document)
 
                     print(verifyClientAddress,verifyClientData,verifyPublicKey)
+                    print("SUCCESS! Updated the data for user '%s' in the database" % self.username)
+                    print('-'*100)
+
                     return self.wfile.write(b'Verifikasjon OK. Du kan naa logge inn')
                 else:
-                    print(sha256(str(rpID+self.getChallenge()).encode()).hexdigest())
+                    print(sha256(str(self.rpID+self.getChallenge()).encode()).hexdigest())
                     print(verifyClientAddress,verifyClientData,verifyPublicKey)
+                    print('-'*100)
                     return self.wfile.write(b'Verifikasjon feilet. ClientData er ikke korrekt')
 
 
@@ -207,7 +223,7 @@ class Handler(http.server.SimpleHTTPRequestHandler):
                     challenge = np.random.randint(0,1000) #TODO generate truly random challenge
 
                     authResponse = {
-                        "rpID": rpID,
+                        "rpID": self.rpID,
                         "credential_ID": userData["credentialID"], #TODO denne er ikke lagret i database enda, må gjøres ved registrering
                         "challenge": challenge   
                     }     
