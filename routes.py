1192155bdf8e419684d6c5f432f4906ae46ab069
Lars Sørensen <larsore@Larss-MacBook-Pro.local>
2023-03-27 16:36:48 +0200

    Flask + react

--- routes.py
+++
@@ -1,5 +0,0 @@
-routes = {
-    "/" : "Hello World! \n " + "Prøv /register for registrering og /auth for autentisering",
-    "/register" : "Her kommer logikk for registrering",
-    "/auth": "Her kommer logikk for autentisering"
-}
